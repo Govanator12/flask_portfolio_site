fe6cc362c7044e82b99a124b17b9e27ef2e24c98
Steven Hardy <steverhardy@gmail.com>
2019-05-05 20:50:16 -0400

    initial commit TODO: add additional html templates

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import Config
+
+# setup app variables
+app = Flask(__name__)
+boostrap = Bootstrap(app)
+app.config.from_object(Config)
+
+# go to routes
+from app import routes
---
+++ app/routes.py
@@ -0,0 +1,22 @@
+from app import app
+from flask import render_template, url_for, redirect, flash
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Home')
+
+
+@app.route('/experience')
+def experience():
+    return render_template('experience.html', title='Experience')
+
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html', title='Projects')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title='Contact')
